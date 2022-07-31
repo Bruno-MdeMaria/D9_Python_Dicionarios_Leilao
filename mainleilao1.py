919d9e5710ab2049e78c8ef67e7910faf7640ffa
Bruno-MdeMaria <brunomartinsdemaria@gmail.com>
2022-07-31 08:21:05 -0300

    Update mainleilao1.py

--- mainleilao1.py
+++ mainleilao1.py
@@ -1,5 +1,22 @@
-
+import os
 from art import logo
 
+dict = {
+
+}
+
+def criar_dados (nome, lance):
+    new =[{}]
+    new["nome"]= nome
+    new["lance"]= lance
+    dict.append(new)
+
+
 print(logo)
+print("\nBem vindo ao programa de leilões secretos!\n")
+nome= input("Qua o seu nome? \n")
+lance= input("Qual o seu lance? \n")
+continua = input("Mais alguém dara um lance? ").lower()
+
+
 
